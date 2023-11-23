3ab33cb8287c3dbdf393e395f64b6aede9501b59
Magomed <megamagomed1@gmail.com>
2023-11-23 14:16:51 +0300

    first commit

---
+++ articles/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Article, Comment
+
+
+class CommentInLine(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
+# Register your models here.
